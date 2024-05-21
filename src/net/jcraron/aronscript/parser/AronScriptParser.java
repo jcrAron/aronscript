d95d262e31b371ca3ed4b4692fad813ff8b91c37
aron jcr <leo2317292762@gmail.com>
2024-05-21 17:52:33 +0800

    read script from file

--- src/net/jcraron/aronscript/parser/AronScriptParser.java
+++ src/net/jcraron/aronscript/parser/AronScriptParser.java
@@ -1,11 +1,36 @@
 package net.jcraron.aronscript.parser;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
 import net.jcraron.aronscript.core.base.FunctionData;
 import net.jcraron.aronscript.parser.script.exception.SyntaxError;
 import net.jcraron.aronscript.parser.script.statements.BlockStatement;
 import net.jcraron.aronscript.util.SubString;
 
 public class AronScriptParser {
+
+	public static FunctionData parseToFunction(File filepath) throws FileNotFoundException, IOException {
+		try {
+			StringBuilder sb = new StringBuilder();
+			try (FileInputStream fileInput = new FileInputStream(filepath)) {
+				try (InputStreamReader reader = new InputStreamReader(fileInput, StandardCharsets.UTF_8)) {
+					while (reader.ready()) {
+						sb.append((char) reader.read());
+					}
+				}
+			}
+			return BlockStatement.toFunction(new SubString(sb.toString()));
+		} catch (SyntaxError e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
 	public static FunctionData parseToFunction(String script) {
 		try {
 			return BlockStatement.toFunction(new SubString(script));
