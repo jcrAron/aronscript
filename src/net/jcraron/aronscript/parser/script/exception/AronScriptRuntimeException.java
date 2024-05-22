58f32abbd9feefb3b95bce48dfffa86e609d34b1
aron jcr <leo2317292762@gmail.com>
2024-05-22 23:41:31 +0800

    Create AronScriptRuntimeException.java

---
+++ src/net/jcraron/aronscript/parser/script/exception/AronScriptRuntimeException.java
@@ -0,0 +1,10 @@
+package net.jcraron.aronscript.parser.script.exception;
+
+public class AronScriptRuntimeException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
